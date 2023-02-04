de95d97c5871eecb2c6484b4eec490580e4dcceb
Mitchell Bourke <mitchjbourke@hotmail.com>
2023-02-04 20:18:51 +1100

    add eventlistener to close nav

--- Function.js
+++ Function.js
@@ -7,3 +7,15 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+
+
+// below this line is the closing section for the hamburger menu//
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
