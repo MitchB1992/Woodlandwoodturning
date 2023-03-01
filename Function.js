43e4f4cce20fd48be493516b51ea1f74b2604299
Mitchell Bourke <mitchjbourke@hotmail.com>
2023-03-01 19:56:18 +1100

    add new navigation for middle section

--- Function.js
+++ Function.js
@@ -19,3 +19,42 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+
+
+
+
+
+function toggleButton() {
+    alert("Hello!")
+}
+
+
+var activeButtonId = null;
+var activeSectionId = null;
+
+function showSection(sectionId, buttonId) {
+    var section = document.getElementById(sectionId);
+    if (section.style.display === "none") {
+        section.style.display = "block";
+        if (activeSectionId !== null && activeSectionId !== sectionId) {
+            document.getElementById(activeSectionId).style.display = "none";
+            document.getElementById(activeButtonId).disabled = false;
+        }
+        activeSectionId = sectionId;
+        activeButtonId = buttonId;
+    } else {
+        section.style.display = "none";
+        document.getElementById(activeButtonId).disabled = false;
+        activeSectionId = null;
+        activeButtonId = null;
+    }
+}
+
+window.addEventListener('load', function() {
+    var sections = document.querySelectorAll('div[id^="new-section"]');
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = "none";
+    }
+});
